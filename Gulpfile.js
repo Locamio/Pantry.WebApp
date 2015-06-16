049814c0764102b41fcb38a1039d3bdb7664566d
Daniel Lewis <danlewis.ltd@gmail.com>
2015-06-17 11:54:42 +1200

    init

---
+++ Gulpfile.js
@@ -0,0 +1,57 @@
+﻿var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    watch = require('gulp-watch'),
+    riot = require('gulp-riot'),
+    ts = require('gulp-typescript');
+
+var config = {
+    //Include all js files but exclude any min.js files
+    src: ['src.js/**/*.js', '!src.js/**/*.min.js'],
+    riot: ['riot.src/**/*.tag']
+}
+
+// Compile TypeScript
+gulp.task('compile', function () {
+    var tsResult = gulp.src('typescript.src/**/*.ts')
+      .pipe(ts({
+          noImplicitAny: true,
+          out: 'src.js/ts.output.js'
+      }));
+    return tsResult.js.pipe(gulp.dest('built/local'));
+});
+
+// Synchronously delete the output file(s)
+gulp.task('clean', function () {
+    del.sync(['build/all.min.js']);
+});
+
+// Combine and minify all files from the app folder
+gulp.task('scripts', ['clean'], function () {
+    return gulp.src(config.src)
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+      .pipe(gulp.dest('build/'));
+});
+
+//Set a default tasks
+gulp.task('default', ['scripts'], function () {
+});
+
+//Set Riot task
+gulp.task('riot', function () {
+    return gulp.src(config.riot)
+        .pipe(riot())
+        .pipe(gulp.dest('build'));
+})
+
+//Set Watch task
+gulp.task('watch', function () {
+    gulp.watch(config.src, ['scripts']);
+    gulp.watch(config.riot, ['riot']);
+});
