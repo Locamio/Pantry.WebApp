d56557dc4a334f7c2662ade3de9d5c037ba0f0b0
Daniel Lewis <danlewis.ltd@gmail.com>
2015-07-09 22:41:16 +1200

    import view

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
 import Cycle from '@cycle/core';
 import CycleWeb from '@cycle/web';
-import ProductComponent from './components/product'
+import ProductComponent from './components/product-item';
+import View from './views/products';
