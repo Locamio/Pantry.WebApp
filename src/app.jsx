0ad7b370f244dacb3942cd99735caab72c058099
Daniel Lewis <danlewis.ltd@gmail.com>
2015-07-09 21:02:23 +1200

    add imports

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,2 @@
-// if (window.location.pathname === '/product') {
-//   showLoadingState();
-//   require.ensure([], function() { // this syntax is weird but it works
-//     hideLoadingState();
-//     require('./product').show(); // when this function is called, the module is guaranteed to be synchronously available.
-//   });
-// } else if (window.location.pathname === '/shop') {
-//   showLoadingState();
-//   require.ensure([], function() {
-//     hideLoadingState();
-//     require('./shop').show();
-//   });
-// }
+import Cycle from '@cycle/core';
+import CycleWeb from '@cycle/web';
